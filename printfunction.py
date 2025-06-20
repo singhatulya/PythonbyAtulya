ea78f5c88d801078a4b0c5e26e96431161d47f5e
Atulya-Singh <97362243+singhatulya@users.noreply.github.com>
2025-06-20 17:52:27 +0530

    Add files via upload

---
+++ printfunction.py
@@ -0,0 +1,40 @@
+#This code will show to use print function
+
+#Use print to show strings (Later discussed)
+
+print("Hello World!")
+
+#Use print to show numbers
+
+print(35)
+
+#Use print to show addition (Don't use quotes)
+
+print(3+6)
+
+#Use print to show variables (Later discussed)
+
+a = 4
+print(a)
+
+#Use print to show addition, subtraction or varibles (Later discussed in detail)
+
+a = 3
+b = 1
+c = 12
+print(12+1-3)
+
+#Use print to show different "terms". You can use as much as you want.
+
+age = 13
+print("Your age is: ", age)
+
+#That was the basics. Here are cool things you can do!
+#Change separtors between "terms" of the print function
+
+print("Apple", "Banana", "Cherry", sep = "|") #Replaces the boring Space to line |
+
+#Customize what to end the print funtion with instead of a new-line.
+
+print("Apple", "Banana", "Cherry", sep = "|", end = "...")
+print("These are fruits!")
