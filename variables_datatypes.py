493e6f810b02b887ce06ef1a74c003995692e799
Atulya-Singh <97362243+singhatulya@users.noreply.github.com>
2025-06-22 11:34:55 +0530

    Variables and Three Data Types

---
+++ variables_datatypes.py
@@ -0,0 +1,37 @@
+#This code will show to use Variables and different ways of storing data(string, integer, float) in Python.
+#--Variables--
+#Variables can be used to store any data. Think of it like a box!
+
+name = "Atulya Singh"
+print(name)
+
+number = 3
+print(number)
+
+pi = 3.14159
+print(pi)
+
+#Now we can manipulate these variables to make computers do anything using these variables.
+
+print("My name is", name) #We don't need to define it now. We have already defined it in line 5.
+r = 3 #Define radius
+print(pi * r * r)   #The * is used for multiplication. This code will find the area of the circle radius r.
+
+#--Data Types--
+#There are three ways we can define data into. Whether it's a string, integer or a float.
+#String data can be used to store letters, words, number and even symbols.
+
+name = str("Atulya Singh!") #We use str for string data
+print("My name is", name)
+
+#Integer data can be used to store integer numbers which could be negative, zero or positive.
+
+students = int(3)
+print("Number of students in class is", students)
+
+#Float amount can be used to store decimal values.
+
+pi = float(3.14159)
+print("Value of pi is", pi)
+
+#Made by Atulya Singh.
